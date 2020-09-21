86478a4c644b9e0c239b091dbfb3887043ea65d7
Cliffart <Cliffart44@gmail.com>
2020-09-21 11:36:27 +0300

    Repository added

--- src/main/java/ru/netology/manager/FilmManager.java
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -1,7 +1,57 @@
 package ru.netology.manager;
 
+import lombok.Data;
+import ru.netology.domain.Film;
+import ru.netology.repository.FilmRepository;
+
+@Data
+
 public class FilmManager {
+    int filmsQuantity = 10;
+    FilmRepository repository;
 
-    public void addFilm
+    public FilmManager(FilmRepository repository) {
+        this.repository = repository;
+    }
 
+    public void add(Film film) {
+        repository.save(film);
+    }
+
+    public Film[] getAll() {
+        Film[] films = repository.findAll();
+        Film[] result = new Film[films.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - 1 - i;
+            result[i] = films[index];
+        }
+        return result;
+    }
+
+    public void removeByID(int id) {
+        repository.removeByID(id);
+    }
+
+    public Film[] getSeveral() {
+        Film[] tmp = getAll();
+        Film[] result = new Film[filmsQuantity()];
+        System.arraycopy(tmp, 0, result, 0, filmsQuantity());
+        return result;
+    }
+
+    public void removeAll() {
+        repository.removeAll();
+    }
+
+    public Film[] findByID(int id) {
+        Film[] result = repository.findByID(id);
+        return result;
+    }
+
+    public Film[] findByArrayID(int id) {
+        Film[] tmp = getAll();
+        Film[] result = new Film[1];
+        System.arraycopy(tmp, id, result, 0, 1);
+        return result;
+    }
 }
---
+++ src/test/java/ru/netology/manager/FilmManagerTest.java
@@ -0,0 +1,117 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.netology.domain.Film;
+import ru.netology.repository.FilmRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class FilmManagerTest {
+    @Mock
+    private FilmRepository repository;
+    @InjectMocks
+    private FilmManager manager;
+    private Film first = new Film(1, "Toy Story", "ts", "Animation", false);
+    private Film second = new Film(2, "Ice Age", "ia", "Animation", false);
+    private Film third = new Film(3, "Inception", "i", "Science fiction", false);
+
+    public void setUp() {
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+    }
+
+    @Test
+    public void shouldGetEmpty() {
+        Film[] returned = new Film[0];
+        doReturn(returned).when(repository).findAll();
+
+        Film[] expected = new Film[0];
+        Film[] actual = manager.getAll();
+        assertArrayEquals(expected, actual);
+
+        verify(repository).findAll();
+    }
+
+    @Test
+    public void shouldRemoveByID() {
+        setUp();
+        int idToRemove = 1;
+        Film[] returned = new Film[]{second, third};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).removeByID(idToRemove);
+
+        manager.removeByID(idToRemove);
+        Film[] expected = new Film[]{third, second};
+        Film[] actual = manager.getAll();
+        assertArrayEquals(expected, actual);
+
+        verify(repository).removeByID(idToRemove);
+    }
+
+    @Test
+    public void shouldFindByID() {
+        setUp();
+        int idToFind = 1;
+        Film[] returned = new Film[]{first};
+        doReturn(returned).when(repository).findByID(idToFind);
+
+
+        Film[] expected = new Film[]{first};
+        Film[] actual = manager.findByID(idToFind);
+        assertArrayEquals(expected, actual);
+
+        verify(repository).findByID(idToFind);
+    }
+
+    @Test
+    public void shouldFindByArrayID() {
+        setUp();
+        int idToFind = 2;
+        Film[] returned = new Film[]{first, second, third};
+        doReturn(returned).when(repository).findAll();
+
+        Film[] expected = new Film[]{first};
+        Film[] actual = manager.findByArrayID(idToFind);
+        assertArrayEquals(expected, actual);
+
+        verify(repository).findAll();
+    }
+
+    @Test
+    public void shouldRemoveAll() {
+        setUp();
+        Film[] returned = new Film[0];
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).removeAll();
+
+        manager.removeAll();
+        Film[] expected = new Film[0];
+        Film[] actual = manager.getAll();
+        assertArrayEquals(expected, actual);
+
+        verify(repository).findAll();
+        verify(repository).removeAll();
+    }
+
+    @Test
+    public void shouldGetSeveral() {
+        setUp();
+        int quantity = 2;
+        Film[] returned = new Film[]{first, second, third};
+        doReturn(returned).when(repository).findAll();
+
+        manager.filmsQuantity(quantity);
+        Film[] expected = new Film[]{third, second};
+        Film[] actual = manager.getSeveral();
+        assertArrayEquals(expected, actual);
+
+        verify(repository).findAll();
+    }
+}
